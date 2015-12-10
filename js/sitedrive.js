9c94759b2055b2d294e7600b8ac7ac68d6264a05
igorzayats <igor.zayatz@gmail.com>
2015-12-10 09:52:04 +0000

    load files

---
+++ js/sitedrive.js
@@ -0,0 +1,69 @@
+﻿function winter () {
+$(document).ready(function(){
+        $("body").css('background-image','url(images/winter.jpg)');
+    });
+}
+function autumn () {
+$(document).ready(function(){
+        $("body").css('background-image','url(images/autumn.jpg)');
+    });
+}
+function spring () {
+$(document).ready(function(){
+        $("body").css('background-image','url(images/spring.jpg)' );
+    });
+}
+
+function summer () {
+$(document).ready(function(){
+        $("body").css('background-image','url(images/summer.jpg)');
+    });
+}
+
+function sea () {
+$(document).ready(function(){
+        $("body").css('background-image','url(images/sea.jpg)');
+    });
+}
+
+function forest () {
+$(document).ready(function(){
+        $("body").css('background-image','url(images/forest.jpg)');
+    });
+}
+
+function lake () {
+$(document).ready(function(){
+        $("body").css('background-image','url(images/lake.jpg)');
+    });
+}
+
+$( "#menubutton" ).click(function() {
+  $( "#list" ).toggle( "fast" );
+});
+
+$( "#backgroundbutton" ).click(function() {
+  $( "#carousel" ).toggle( "fast" );
+});
+$( "#radio" ).click(function() {
+  $( "#radiotable" ).fadeToggle( "fast" );
+});
+
+
+$( "#sevendays" ).click( function hide16 () {
+     $("#weatherTable16 table").remove();
+    $( "#blockweather16" ).css('display', 'none');
+    
+    $( "#blockweather5" ).css('display', 'block');
+   
+});
+
+$( "#sixteenthdays" ).click( function hide5 () {
+     $("#weatherTable5 table").remove();
+    $( "#blockweather5" ).css('display', 'none');
+    $( "#blockweather16").css('display', 'block');
+});
+
+$( "#menubutton" ).click( function showAllweather () {
+    $( "#allweather" ).css('display', 'block');
+    });
---
+++ js/weatherByCity5.js
@@ -0,0 +1,50 @@
+﻿$('#btnGetWeather5').click(function () {
+        getWeatherByCity5('ua', $('#inputCityName5').val());
+    });
+    $('#inputCityName5').keypress(function(e) {
+        var ENTER_KEY_CODE = 13;
+        if ( e.which === ENTER_KEY_CODE ) 
+        {
+            $('#btnGetWeather5').trigger('click');
+            return false;
+        }
+    });  
+function getWeatherByCity5(lang,city) {
+$.getJSON(
+        'http://api.openweathermap.org/data/2.5/forecast?q=' + city + '&mode=json' +'&APPID=7fc874cf9bdf419b09e618fc662fcc47&cnt=40&units=metric&lang=' + lang + '&callback=?',
+        function (data) {
+             var text;
+                    $('#raw_json pre').text(JSON.stringify(data));
+ $("#weatherTable5 tr:not(:first)").remove();
+                    
+                   
+                     
+                        $.each(data.list, function(){ 
+                      addWeather5(this.dt,  this.weather[0].icon,  this.weather[0].description, 
+                       Math.round(this.main.temp),
+                     
+                      Math.round(this.main.pressure*0.75006375541921),
+                      this.main.humidity);
+    
+                          });
+                          
+                                       
+        });
+}
+                      
+                     
+                    function addWeather5(date, icon, description, temp, pressure, humidity){
+        var weather = '<tr>'+
+                '<td>' + date + '</td>' +
+                '<td>' +  icon + '</td>' +
+                '<td>' + description + '</td>' +
+                '<td>' + temp + '</td>' +
+                '<td>' + pressure + '</td>' +
+                '<td>' + humidity + '</td>' +
+                '</tr>';
+        document.getElementById('weatherTable5').insertRow(-1).innerHTML = weather; 
+    }
+                      
+                          
+  
+            
