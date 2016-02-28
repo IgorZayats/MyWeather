c6b9d0d117a465365c7e1d437685ce89d260be83
IgorZayats <igor.zayatz@gmail.com>
2016-02-28 10:32:35 +0200

    Update radio.js

--- js/radio.js
+++ js/radio.js
@@ -11,21 +11,21 @@
             var stream = response[3].streams[0].stream;
             $("#player2").toggle('fast');
             $("#player2").css('display', 'block');
-            $("#player2").append("<source src=" + stream + "/;" + "></source>");
+            $("#player2").append("<source src=" + stream  + "></source>");
         });
 
         $("#renesans").on("click", function djfm() {
             var stream = response[4].streams[0].stream;
             $("#player3").toggle('fast');
             $("#player3").css('display', 'block');
-            $("#player3").append("<source src=" + stream + "/;" + "></source>");
+            $("#player3").append("<source src=" + stream + "></source>");
         });
 
         $("#sharmanka").on("click", function djfm() {
             var stream = response[5].streams[0].stream;
             $("#player4").toggle('fast');
             $("#player4").css('display', 'block');
-            $("#player4").append("<source src=" + stream + "/;" + "></source>");
+            $("#player4").append("<source src=" + stream + "></source>");
         });
 
         $("#transcentury").on("click", function djfm() {
