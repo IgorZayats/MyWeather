68e07e7deb2b2173f6a11eb419dfe3196f4ef889
IgorZayats <igor.zayatz@gmail.com>
2016-02-28 10:40:08 +0200

    Update radio.js

--- js/radio.js
+++ js/radio.js
@@ -8,14 +8,14 @@
         });
 
         $("#djfm").on("click", function djfm() {
-            var stream = response[3].streams[0].stream;
+            var stream = response[18].streams[0].stream;
             $("#player2").toggle('fast');
             $("#player2").css('display', 'block');
             $("#player2").append("<source src=" + stream  + "></source>");
         });
 
         $("#renesans").on("click", function djfm() {
-            var stream = response[4].streams[0].stream;
+            var stream = response[9].streams[0].stream;
             $("#player3").toggle('fast');
             $("#player3").css('display', 'block');
             $("#player3").append("<source src=" + stream + "></source>");
@@ -29,21 +29,21 @@
         });
 
         $("#transcentury").on("click", function djfm() {
-            var stream = response[10].streams[0].stream;
+            var stream = response[17].streams[0].stream;
             $("#player5").toggle('fast');
             $("#player5").css('display', 'block');
             $("#player5").append("<source src=" + stream + "></source>");
         });
 
         $("#comafm").on("click", function djfm() {
-            var stream = response[11].streams[0].stream;
+            var stream = response[16].streams[0].stream;
             $("#player6").toggle('fast');
             $("#player6").css('display', 'block');
             $("#player6").append("<source src=" + stream + "></source>");
         });
 
         $("#mfm").on("click", function djfm() {
-            var stream = response[14].streams[0].stream;
+            var stream = response[13].streams[0].stream;
             $("#player8").toggle('fast');
             $("#player8").css('display', 'block');
             $("#player8").append("<source src=" + stream + "></source>");
